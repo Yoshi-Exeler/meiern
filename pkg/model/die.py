2ca178eff18945d9bfd3c9e69d41dd788603e4f7
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2021-02-05 16:02:01 +0100

    Implement Die

--- pkg/model/die.py
+++ pkg/model/die.py
@@ -1,3 +1,10 @@
+from random import randrange
+
 class Die:
-    def __init__(self) -> None:
-        pass
+    __sides: int
+
+    def __init__(self,sides) -> None:
+        self.__sides = sides
+
+    def Roll(self) -> int:
+        return randrange(1,self.__sides)
