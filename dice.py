036dcc345282083586fe5960aa5b31ffd9f36049
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2021-02-05 16:25:19 +0100

    Implement Dice

--- dice.py
+++ dice.py
@@ -10,3 +10,11 @@ class Dice:
         for die in self.__dice:
             die.Roll()
 
+    def GetValue(self) -> int:
+        prim = max(self.__dice[0],self.__dice[1])
+        sec = min(self.__dice[0],self.__dice[1])
+        return 10*prim+sec
+    
+    def Bigger(self,other_dice) -> bool:
+        return self.GetValue() > other_dice.GetValue()
+
